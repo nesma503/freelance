6cc3428862d3ed183b89fd214e8deecc3ef287f7
Siham <siham.rahal@outlook.com>
2021-09-18 21:05:24 +0300

    refactoring

--- pages/user-profile.php
+++ pages/user-profile.php
@@ -1,75 +1,92 @@
 <?php
-session_start();
-require_once "../db/dbWrapper.php";
+require_once "../models/user.php";
+require_once "../models/freelancer.php";
+require_once "../models/recruiter.php";
 require_once "../helpers/helpers.php";
+session_start();
 
-$db = new dbWrapper();
+if (!isset($_SESSION['recruiter']) && !isset($_SESSION['freelancer']))
+  header('Location: ./login.php');
+else if (isset($_SESSION['recruiter'])) {
+  $user = $_SESSION['recruiter'];
+  $recruiter = $_SESSION['recruiter'];
+} else {
+  $user = $_SESSION['freelancer'];
+  $freelancer = $_SESSION['freelancer'];
+}
 
-// $user = $_SESSION["user"];
-// $firstName = $user->FirstName;
-// $lastName = $user->LastName;
-// $username = $user->Username;
-// $email = $user->Email;
-// $DOB = $user->DOB;
-// $userId = $user->ID;
-
+$userId = $user->UserId;
+$firstName = $user->FirstName;
+$lastName = $user->LastName;
+$username = $user->Username;
+$email = $user->Email;
+$DOB = $user->DOB;
+$userType = $user->UserTypeId;
+$msg = $error = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $firstName = $_POST["firstName"];
-  $lastName = $_POST["lastName"];
-  $username = $_POST["username"];
-  $email = $_POST["email"];
-  $DOB = $_POST["DOB"];
-  $userId = $_SESSION['userId'];
-
+  $firstName = trim($_POST["firstName"]);
   if (empty($firstName)) {
-    $firstName_err = "Please enter first name.";
+    $firstName_err = "Please enter your first name.";
   }
 
+  $lastName = trim($_POST["lastName"]);
   if (empty($lastName)) {
-    $lastName_err = "Please enter last name.";
+    $lastName_err = "Please enter your last name.";
   }
 
+  $username = trim($_POST["username"]);
   if (empty($username)) {
-    $username_err = "Please enter username.";
+    $username_err = "Please enter a username.";
   }
 
-  $sql_u = "SELECT * FROM users WHERE username= ? where userId <> ?";
-  $exists = $db::queryOne($sql_u, [$username, $userId]);
-  if ($exists > 0) {
+  // check if the username not exist
+  $valid = User::ValidateUsername($username, $userId);
+  if (!$valid) {
     $username_err = "Username already taken";
   }
 
+  $email = trim($_POST["email"]);
   if (empty($email)) {
-    $email_err = "Please enter email.";
+    $email_err = "Please enter your email.";
   }
 
+  $DOB = trim($_POST["DOB"]);
   if (empty($DOB)) {
-    $username_err = "Please enter date of birth.";
+    $DOB_err = "Please enter your date of birth.";
   }
 
 
-  if (empty($firstName_err) && empty($lastName_err) && empty($username_err) && empty($email_err) && empty($DOB)) {
-    
+  if (empty($firstName_err) && empty($lastName_err) && empty($username_err) && empty($email_err) && empty($DOB_err)) {
+
     $user = new User();
-    $user->ID = $userId;
+    $user->Id = $userId;
     $user->FirstName = $firstName;
     $user->LastName = $lastName;
     $user->Username = $username;
     $user->Email = $email;
     $user->DOB = $DOB;
 
-    $user->UpdateUser();
-    $sql = "update users set FirstName=?, LastName=?, username=?, email=?, DOB=?  where id=?";
-    $update = $db::query($sql, [$firstName, $lastName, $username, $email, $DOB, $userId]);
+    $success = $user->Save();
+    if ($success) {
 
-    if ($update == 1) {
-      $msg = "Information saved!";
-      $sql = "select * from users where id=?";
-      $user = $db::queryOne($sql);
-      $_SESSION["user"] = $user;
+      if ($userType == 1) {
+        $freelancer = Freelancer::LoadByUserId($userId);
+        if ($freelancer != null) {
+          $_SESSION["freelancer"] = $freelancer;
+          $msg = "Information saved!";
+        } else
+          $error = "An error occured!";
+      } else {
+        $recruiter = Recruiter::LoadByUserId($userId);
+        if ($recruiter != null) {
+          $_SESSION["recruiter"] = $recruiter;
+          $msg = "Information saved!";
+        } else
+          $error = "An error occured!";
+      }
     } else
-      $msg = "An error occured!";
+      $error = "An error occured!";
   }
 }
 
@@ -84,125 +101,71 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>User Profile</title>
   <link rel="stylesheet" href="../css/bootstrap.min.css">
-  <link rel="stylesheet" href="../css/icons.css">
+  <link rel="stylesheet" href="../css/menu.css">
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/bootstrap-datepicker.min.css">
 
+  <script src="../js/jquery-3.6.0.js"></script>
+  <script src="../js/bootstrap-datepicker.min.js"></script>
 
-<style>
-        .aside {
-            border-right: 1px solid #ECEDF2;
-            box-shadow: 0 6px 15px 0 rgb(64 79 104 / 5%);
-        }
-
-        img {
-            width: 80px;
-            margin: 20px;
-        }
-
-        .menu {
-            list-style: none;
-        }
-
-        li {
-            padding: 10px;
-            text-decoration: none;
-            margin-bottom: 2px;
-            box-sizing: border-box;
-            display: list-item;
-        }
-
-        li a {
-            color: #77838F;
-            font-size: 18px;
-            font-weight: 400;
-            line-height: 1.75;
-        }
-
-        .menu {
-            list-style: none;
-            padding: 0;
-            margin: 0
-        }
-
-        .menu li {
-            color: #696969;
-            background-color: #fff;
-            padding: 4px 15px;
-            display: inline-block;
-            width: 100%;
-            margin-bottom: 2px
-        }
-
-        .menu li a span {
-            padding: 10px;
-        }
-
-        .menu li a span:hover,
-        .menu li a .active {
-            border-radius: 8px;
-            width: 100%;
-            color: #1967D2;
-            background-color: rgba(25, 103, 210, 0.1);
-        }
-
-        .menu li a span {
-            display: inline-block;
-            font-size: 18px;
-            margin-right: 10px
-        }
-    </style>
-
+  <script>
+    $(function() {
+      $('.datepicker').datepicker();
+    });
+  </script>
 </head>
 
-
 <body>
-  <div class="row">
-    <?php menu(1,"profile", "siham") ?>
-    <div class="col-9">
-      <div class="profile-form-wrapper">
-        <h1 class="d-flex justify-content-center">Edit Profile</h1>
-        <div class="inner-list form-group">
-          <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-            <div>
-              <h3 class="d-flex justify-content-center">My Profile</h3>
+  <div class="container-fluid">
+    <div class="row">
+      <!-- menu -->
+      <div class="col-3 z-index-2">
+        <?php menu($userType, "profile", $user->Username); ?>
+      </div>
+      <!-- page -->
+      <div class="col-9 wrapper">
+        <div class="container-fluid">
+          <div class="title">
+            <h2>Edit Profile</h2>
+          </div>
+          <form class="form" action="<?php echo ($_SERVER["PHP_SELF"]); ?>" method="POST">
+            <?php if ($error != "") echo '<div class="alert alert-danger p-1 mt-1">' . $error . '</div>'; ?>
+            <?php if ($msg != "") echo '<div class="alert alert-success p-1 mt-1">' . $msg . '</div>'; ?>
+            <div class="form-group">
+              <div class="row">
+                <div class="col">
+                  <label for="firstName">First Name*</label>
+                  <input type="text" id="firstName" name="firstName" class="form-control <?php if ($firstName_err != "") echo 'is-invalid'; ?>" required value="<?php echo $firstName; ?>">
+                  <span class="invalid-feedback"><?php echo $firstName_err; ?></span>
+                </div>
+                <div class="col">
+                  <label for="lastName">Last Name*</label>
+                  <input type="text" id="lastName" name="lastName" class="form-control <?php if ($lastName_err != "") echo 'is-invalid'; ?>" required value="<?php echo $lastName; ?>">
+                  <span class="invalid-feedback"><?php echo $lastName_err; ?></span>
+                </div>
+              </div>
             </div>
 
-            <div class="form-group">
-              <label for="firstName">First Name</label>
-              <input type="text" class="form-control" name="firstName" placeholder="Enter first name" value="<?php echo $firstName; ?>">
+            <div class=" form-group">
+              <label for="username">Username*</label>
+              <input type="text" id="username" name="username" class="form-control <?php if ($username_err != "") echo 'is-invalid'; ?>" required value="<?php echo $username; ?>">
+              <span class="invalid-feedback"><?php echo $username_err; ?></span>
             </div>
 
-            <div class="form-group">
-              <label for="lastName">Last Name</label>
-              <input type="text" class="form-control" name="lastName" placeholder="Enter first Name" value="<?php echo $lastName; ?>>
+            <div class=" form-group">
+              <label for="email">Email Address*</label>
+              <input type="email" id="email" name="email" class="form-control <?php if ($email_err != "") echo 'is-invalid'; ?>" required value="<?php echo $email; ?>">
+              <span class="invalid-feedback"><?php echo $email_err; ?></span>
             </div>
 
-            <div class="form-group">
-              <label for="username">Username</label>
-              <input type="text" class="form-control" name="username" placeholder="Enter username" value="<?php echo $username; ?>>
-
+            <div class=" form-group">
+              <label for="DOB">Date of Birth*</label>
+              <input type="text" id="DOB" name="DOB" class="datepicker form-control <?php if ($DOB_err != "") echo 'is-invalid'; ?>" required data-date-format="yyyy-mm-dd" value="<?php echo $DOB ?>">
+              <span class="invalid-feedback"><?php echo $DOB_err; ?></span>
             </div>
-
-            <div class="form-group">
-              <label for="email">Email address</label>
-              <input type="email" class="form-control" name="email" placeholder="Enter email" value="<?php echo $email; ?>>
-
-            </div>
-
-            <div class="form-group">
-              <label for="exampleInputEmail1">Date of birth</label>
-              <input type="email" class="form-control" name="DOB" placeholder="Enter date of birth" value="<?php echo $DOB; ?>>
-            </div>
-            <div class="form-group">
+            <div class=" form-group">
               <input type="submit" class="btn btn-primary btn-lg btn-block" value="Update">
             </div>
-
-
-
-
-
-
-
-
           </form>
         </div>
       </div>
